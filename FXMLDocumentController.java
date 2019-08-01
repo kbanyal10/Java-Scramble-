18ee217407adc75416a2ba5c412e43a97f49921b
kbanyal10 <39594518+kbanyal10@users.noreply.github.com>
2019-08-01 19:50:29 -0400

    Add files via upload

--- FXMLDocumentController.java
+++ FXMLDocumentController.java
@@ -6,11 +6,21 @@
 package assignment2_200395834;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
 
 /**
  *
@@ -19,17 +29,79 @@ import javafx.scene.control.Label;
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Label label;
+    private TextField typeHere;
     
+    @FXML
+    private TextField points;
     
      @FXML
-    private Label previousButton;
-     
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private TextArea oldWords;
+    
+    
+    ArrayList<String> mylist = new ArrayList<>();
+     String value =  (typeHere.getText()).toLowerCase();
+    
+    public void showPoints(){
+       
+        
+        if((value.contains("a")||value.contains("e")||value.contains("i")||value.contains("o")|| 
+            value.contains("u")||value.contains("y"))&&((value.length()>= 2)&&(value.length()<= 8))) {
+       
+        
+        mylist.add(value);
+        String[] words = value.split("");
+        oldWords.setText(mylist.toString());
+        points.setText(Arrays.toString(words));
+        }
+        
+       else {
+                
+                
+                Stage dialog = new Stage();
+                dialog.initModality(Modality.APPLICATION_MODAL);
+                dialog.setTitle("TestRun");
+                dialog.setMinHeight(300);
+                dialog.setMinWidth(300);
+                Label label = new Label();
+                label.setText("ALERT");
+                Button closeButton = new Button("You have entered wrong input");
+                closeButton.setOnAction(e-> dialog.close());
+                VBox layout = new VBox(10);
+                layout.getChildren().addAll(label, closeButton);
+                layout.setAlignment(Pos.CENTER);
+                Scene scene = new Scene(layout);
+                dialog.setScene(scene);
+                dialog.showAndWait();
+        }
+        
+        
+         
+        
+        
+        
+        
     }
+//    public boolean comparingWords(){
+//        
+//        for (int i=0; i<= mylist.size(); i++) {
+//            
+//        
+//            if(mylist.contains(value)){
+//                
+//                return false;
+//            }
+//           
+//        }
+//        return true;
+//        
+//        }
+   
+     
+//    @FXML
+//    private void handleButtonAction(ActionEvent event) {
+//        System.out.println("You clicked me!");
+//        
+//    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
