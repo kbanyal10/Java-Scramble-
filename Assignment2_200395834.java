e13fc45c765fc4528e547d20212e323fd979d36c
kbanyal10 <39594518+kbanyal10@users.noreply.github.com>
2019-08-09 17:03:53 -0400

    Add files via upload

--- Assignment2_200395834.java
+++ Assignment2_200395834.java
@@ -6,7 +6,6 @@
 package assignment2_200395834;
 
 import javafx.application.Application;
-import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
